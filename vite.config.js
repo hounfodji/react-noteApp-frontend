94b18d0dfed30f82b02bc51a490e08d083803ed9
hounfodji <hospicehounfodji@gmail.com>
2024-03-31 15:43:07 +0100

    add deploy workflow change base

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/react-noteApp-frontend/",
+  base: "https://hounfodji.github.io/react-noteApp-frontend/",
 })
