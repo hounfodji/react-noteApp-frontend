883b1ba3f6a17a979f4a19a76f95f77ea85c6714
hounfodji <hospicehounfodji@gmail.com>
2024-03-31 15:33:14 +0100

    add deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/react-noteApp-frontend/",
   plugins: [react()],
+  base: "/react-noteApp-frontend/",
 })
